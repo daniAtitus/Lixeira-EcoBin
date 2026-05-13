98cd082bc14f4f96bf9093321e75831a8c2ae57f
DANIEL MACHADO BORGES <1135956@ATITUS.LOCAL>
2026-05-13 19:59:23 -0300

    Funcionando cadastro de lixeira

---
+++ client/fetch-lixeira/lixeira.js
@@ -0,0 +1,43 @@
+  const form = document.getElementById("formCadastroLixeira");
+
+  form.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const dados = {
+      nome: document.getElementById("nome").value,
+      identificador_unico: document.getElementById("identificador_unico").value,
+      endereco: document.getElementById("endereco").value,
+      latitude: parseFloat(document.getElementById("latitude").value),
+      longitude: parseFloat(document.getElementById("longitude").value),
+      capacidade_total_estimada: parseFloat(
+        document.getElementById("capacidade_total_estimada").value
+      ),
+      nivel_atual_ocupacao: parseFloat(
+        document.getElementById("nivel_atual_ocupacao").value
+      ),
+      status_tampa: document.getElementById("status_tampa").value,
+      status_operacional: document.getElementById("status_operacional").value
+    };
+
+    try {
+      const response = await fetch("http://localhost:8080/lixeiras", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dados)
+      });
+
+      if (response.ok) {
+        alert("Lixeira cadastrada com sucesso!");
+
+        form.reset();
+      } else {
+        alert("Erro ao cadastrar lixeira");
+      }
+
+    } catch (erro) {
+      console.error("Erro:", erro);
+      alert("Erro na requisição");
+    }
+  });
