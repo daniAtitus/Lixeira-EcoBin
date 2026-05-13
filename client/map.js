ae91bc5e02d0b315ebcaffeb35cae06d41e15c9f
DANIEL MACHADO BORGES <1135956@ATITUS.LOCAL>
2026-05-12 22:23:53 -0300

    Testando configuração do mapa

--- client/map.js
+++ client/map.js
@@ -1,22 +1,37 @@
+const map = L.map('mapa-central').setView([-28.3875, -53.9147], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap'
+}).addTo(map);
+
 async function buscaLixeiras() {
-    const rota = "http://localhost:8080/lixeiras"
+  const rota = "http://localhost:8080/lixeiras";
 
-    try {
-        const response = await fetch(rota)
+  const response = await fetch(rota);
 
-        if (!response.ok) {
-            throw new Error(`Erro HTTP: ${response.status}`)
-        }
+  if (!response.ok) {
+    throw new Error(`Erro HTTP: ${response.status}`);
+  }
 
-        const dados = await response.json()
-        return dados
-    } catch (error) {
-        console.error("Erro ao buscar lixeiras:", error)
-    }
+  return await response.json();
 }
 
 async function init() {
-    const teste = await buscaLixeiras()
-    console.log(teste)
+  const lixeiras = await buscaLixeiras();
+
+  lixeiras.forEach(lixeira => {
+    const lat = lixeira.latitude;
+    const lng = lixeira.longitude;
+
+    L.marker([lat, lng])
+      .addTo(map)
+      .bindPopup(`
+        <b>${lixeira.nome}</b><br>
+        Ocupação: ${lixeira.nivel_atual_ocupacao}%<br>
+        Tampa: ${lixeira.status_tampa}<br>
+        Status: ${lixeira.status_operacional}
+      `);
+  });
 }
-init()
+
+init();
